ee71b8313b992f3f5894924912492b39374bc09e
chrisvriend <59793342+chrisvriend@users.noreply.github.com>
2022-06-02 14:19:00 +0200

    Update segmentQA2html.py
    omitted contour display

--- segmentQA2html.py
+++ segmentQA2html.py
@@ -37,31 +37,34 @@ view=plotting.view_img(seg,
                        bg_img=brain,symmetric_cmap=False,
                        resampling_interpolation='nearest',
                        colorbar=False,opacity=0.6,annotate=True,
-                       cmap='CMRmap')
+                       cmap='Set2')
 #view.save_as_html(output + '.html')
 html1=view.get_standalone()
 html1=html1.replace('width="600"','width="2400"')
 html1=html1.replace('height="240"','width="960"')
 
 # make figure of segmentation contours
-display = plotting.plot_anat(brain, display_mode='tiled',draw_cross=False,)
-display.add_contours(seg,alpha=0.5,levels=[0.5],colors='r',linewidths=0.5)
+#display = plotting.plot_anat(brain, display_mode='tiled',draw_cross=False,)
+#display.add_contours(seg,alpha=0.5,levels=[0.5],colors='r',linewidths=0.5)
 
-display.savefig(output + '.png',dpi=300)
+#display.savefig(output + '.png',dpi=300)
 
 # convert to html code
-data_uri = base64.b64encode(open(output + '.png', 'rb').read()).decode('utf-8')
-img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
+#data_uri = base64.b64encode(open(output + '.png', 'rb').read()).decode('utf-8')
+#img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
 
 # clean up
-os.remove(output + '.png')
+#os.remove(output + '.png')
 
 
 htmlbase='<!DOCTYPE html> <html lang="en"> <head> <title>Slice viewer</title>  <meta charset="UTF-8" /> </head> <body>'
 htmlend='</body> </html>'
 
 
-htmlfull=htmlbase + html1 + img_tag + htmlend
+#htmlfull=htmlbase + html1 + img_tag + htmlend
+htmlfull=htmlbase + html1 + htmlend
+
+
 
 # Write HTML String to file.html
 with open(output + ".html", "w") as file:
